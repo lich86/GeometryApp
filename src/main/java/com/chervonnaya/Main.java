aca1f3f2e2b300bca9748c08289384f7ba4003ed
lich <elizaveta.chervonnaya@gmail.com>
2024-07-19 14:02:13 +0300

    changed GeometryUtils version to 1.1.0-SNAPSHOT

--- src/main/java/com/chervonnaya/Main.java
+++ src/main/java/com/chervonnaya/Main.java
@@ -13,5 +13,18 @@ public class Main {
         Circle circle = new Circle(5);
         System.out.println(circle.getArea());
         System.out.println(circle.getPerimeter());
+
+        if(CompareUtil.compareFiguresByPerimeter(circle, triangle) > 0) {
+            System.out.println("Периметр первой фигуры больше периметра второй фигуры");
+        } else {
+            System.out.println("Периметр второй фигуры больше периметра первой фигуры");
+        }
+
+        if(CompareUtil.compareFiguresByArea(triangle, rectangle) > 0) {
+            System.out.println("Площадь первой фигуры больше площади второй фигуры");
+        } else {
+            System.out.println("Площадь второй фигуры больше площади первой фигуры");
+        }
+
     }
 }
